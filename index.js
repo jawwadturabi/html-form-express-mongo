55a70d4d60011b58abe20a3a1d3c538a8a0b479a
Jawwad Turabi <memonjawwad9@gmail.com>
2019-05-22 17:41:04 +0500

    more better

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
-const dburi = "mongodb+srv://author:author123@cluster0-geoiq.mongodb.net/test?retryWrites=true";
+const alert = require('alert-node');
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express()
+const dburi = "mongodb+srv://author:author123@cluster0-geoiq.mongodb.net/test?retryWrites=true";
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -23,31 +24,50 @@ mongoose.connection.on("disconnected", () => {
 });
 
 var nameSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String
+    UserName: String,
+    Password: 'String'
 });
 var User = mongoose.model("User", nameSchema);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+app.get("/signup", (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
+})
 
-app.post("/addname", async (req, res) => {
-    console.log("request is", req.body)
-    var info= {
-        firstName : req.body.firstName,
-        lastName : req.body.lastName
+app.post("/login", async (req, res) => {
+    User.find({ UserName: req.body.UserName, Password: req.body.Password }).then(d => {
+        if (d.length == 0) {
+            alert("Signup First")
+            res.sendFile(__dirname + "/index.html");
+        }
+        else {
+            res.send("Login Successful")
+            console.log("data is : ", d)
+        }
+    }).catch(e => {
+        res.send("Something went wrong !")
+        console.log("err is : ", e)
+    })
+})
+
+app.post("/signup", async (req, res) => {
+    var info = {
+        UserName: req.body.UserName,
+        Password: req.body.Password
     }
     var saveData = new User(info);
     await saveData.save()
         .then(item => {
-            res.send(200, "Name saved to database");
+            res.send("Signup Successfull");
             console.log("data is  : ", item)
         })
         .catch(err => {
+            console.log("err is : ", err)
             res.send(200, "Unable to save to database");
         });
-});
-app.listen(3000,()=>{
+})
+app.listen(3000, () => {
     console.log("server running")
 })
